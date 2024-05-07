edcbaab02bc55ad778ac9ca926888493f27d27f6
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-07 20:18:32 +0530

    first commit

--- Learn/Banchi/first_javafx_learn/src/main/java/org/example/Main.java
+++ Learn/Banchi/first_javafx_learn/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
