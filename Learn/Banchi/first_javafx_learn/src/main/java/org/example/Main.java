89e8c78b3b13783b31e672eb41205ddd11a2f00c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-07 20:20:03 +0530

    first commit

--- Learn/Banchi/first_javafx_learn/src/main/java/org/example/Main.java
+++ Learn/Banchi/first_javafx_learn/src/main/java/org/example/Main.java
@@ -1,12 +1,19 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        Scene scene=new Scene(FXMLLoader.load(getClass().getResource("/views/testView.fxml")));
+        stage.setScene(scene);
+        stage.setTitle("tharindu's view");
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
     }
 }
