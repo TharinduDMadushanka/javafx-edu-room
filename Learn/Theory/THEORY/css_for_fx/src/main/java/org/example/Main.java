e9094420208351ad88485ef2250d890a2f276a24
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-08 15:50:27 +0530

    first commit

--- Learn/Theory/THEORY/css_for_fx/src/main/java/org/example/Main.java
+++ Learn/Theory/THEORY/css_for_fx/src/main/java/org/example/Main.java
@@ -16,6 +16,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Scene scene=new Scene(FXMLLoader.load(getClass().getResource("/view/css.fxml")));
+        //scene.getStylesheets().add(getClass().getResource("app.css").toExternalForm()); //link css file
+
+        String css =this.getClass().getResource("app.css").toExternalForm(); // for more than one file
+        scene.getStylesheets().add(css);
 
         Group root= new Group();
         Stage stage2=new Stage();
