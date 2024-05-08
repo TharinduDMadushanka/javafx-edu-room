977544e4823bc116279c6758830b2746f4be5acb
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-08 10:34:50 +0530

    first commit

---
+++ Learn/Theory/src/main/java/org/example/LearnScene.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LearnScene extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
