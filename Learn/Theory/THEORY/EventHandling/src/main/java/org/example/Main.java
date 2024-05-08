30a6a8a005920deaf5826afcb6ef5db03cc0b800
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-08 15:01:00 +0530

    first commit

--- Learn/Theory/THEORY/EventHandling/src/main/java/org/example/Main.java
+++ Learn/Theory/THEORY/EventHandling/src/main/java/org/example/Main.java
@@ -1,16 +1,20 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws IOException {
+        Scene scene=new Scene(FXMLLoader.load(getClass().getResource("/view/eventHandle.fxml")));
+
         Group root= new Group();
-        Scene scene=new Scene(root);
         Stage stage2=new Stage();
 
         stage2.setScene(scene);
--- Learn/Theory/THEORY/EventHandling/src/main/java/org/example/controller/EventControllert.java
+++ Learn/Theory/THEORY/EventHandling/src/main/java/org/example/controller/EventControllert.java
@@ -1,4 +1,35 @@
 package org.example.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+
+import java.awt.event.ActionEvent;
+
 public class EventControllert {
+
+    @FXML
+    private Circle myCircle;
+    private double x;
+    private double y;
+
+    public void up(javafx.event.ActionEvent event ){
+        //System.out.println("Up");
+        myCircle.setCenterY(y-=10);
+    }
+
+    public void down(javafx.event.ActionEvent event ){
+        //System.out.println("Down");
+        myCircle.setCenterY(y+=10);
+    }
+
+    public void left(javafx.event.ActionEvent actionEvent) {
+        //System.out.println("Left");
+        myCircle.setCenterY(x-=10);
+    }
+
+    public void right(javafx.event.ActionEvent event ){
+        //System.out.println("Right");
+        myCircle.setCenterY(x+=10);
+    }
+
 }
