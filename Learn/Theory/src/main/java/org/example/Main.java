ea20352253987bd2ae6846ec4e70d97536880875
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-08 09:48:09 +0530

    first commit

---
+++ Learn/Theory/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene=new Scene(FXMLLoader.load(getClass().getResource("/views/page1.fxml")));
+    }
+}
