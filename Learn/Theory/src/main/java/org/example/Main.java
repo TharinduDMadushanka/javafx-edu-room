a4abd4de4085243969f944323d0441391e511506
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-08 10:32:41 +0530

    first commit

--- Learn/Theory/src/main/java/org/example/Main.java
+++ Learn/Theory/src/main/java/org/example/Main.java
@@ -2,19 +2,47 @@ package org.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene=new Scene(FXMLLoader.load(getClass().getResource("/views/page1.fxml")));
+
+        Group root= new Group(); // create roo node
+        Scene scene=new Scene(root, Color.BLACK);// add scene and change background color to black
+
+//        Image icon =new Image("image.png");
+//        stage.getIcons().add(icon);
+
+        stage.setTitle("Hello Fx");
+        stage.setResizable(false);//  resizability
+
+        /**
+         * set escape full screen --> press "q"
+         */
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("Press 'q' to escape");
+        stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+
+        stage.setWidth(420);
+        stage.setHeight(420);
+//        stage.setX(50); ---> change opening position of window
+//        stage.setY(50);
 
         stage.setScene(scene);
-        stage.setTitle("tharindu's view");
-        stage.centerOnScreen();
-        stage.setResizable(false);
+
+
         stage.show();
     }
 }
