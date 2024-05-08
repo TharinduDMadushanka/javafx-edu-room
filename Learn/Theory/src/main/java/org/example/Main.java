cef906dbc665087c0bf614257dc72133731e8e72
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-08 09:56:59 +0530

    first commit

--- Learn/Theory/src/main/java/org/example/Main.java
+++ Learn/Theory/src/main/java/org/example/Main.java
@@ -10,5 +10,11 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Scene scene=new Scene(FXMLLoader.load(getClass().getResource("/views/page1.fxml")));
+
+        stage.setScene(scene);
+        stage.setTitle("tharindu's view");
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
     }
 }
