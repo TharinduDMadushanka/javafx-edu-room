d375dfcdc6ef6addfb248d943c92e272cc956b72
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-08 08:27:59 +0530

    second commit

--- Learn/Banchi/first_javafx_learn/src/main/java/org/example/PageTwoControler/ControllerTwo.java
+++ Learn/Banchi/first_javafx_learn/src/main/java/org/example/PageTwoControler/ControllerTwo.java
@@ -1,4 +1,25 @@
 package org.example.PageTwoControler;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ControllerTwo {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void goPageOne(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/testView.fxml"));
+        stage=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
