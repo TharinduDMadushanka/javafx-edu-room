cf90ac9430feb97eee1f0071607fb3e2dbaf8cd9
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-23 19:00:28 +0530

    test javafx project

--- Test/Test1/src/main/java/lk/practice/controller/MainFormController.java
+++ Test/Test1/src/main/java/lk/practice/controller/MainFormController.java
@@ -1,6 +1,10 @@
 package lk.practice.controller;
 
+import javafx.scene.control.Label;
+
 public class MainFormController {
+
+    public Label lblName;
 
     public MainFormController() {
         System.out.println("MainFormController Created");
