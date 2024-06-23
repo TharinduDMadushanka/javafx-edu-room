dc618077453e5a29618077bdcc2c7cddb5c5a41f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-23 18:40:17 +0530

    test javafx project

---
+++ Test/Test1/src/main/java/lk/practice/Appinitializer.java
@@ -0,0 +1,24 @@
+package lk.practice;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Appinitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainForm.fxml"));
+
+        Parent load =loader.load();
+
+    }
+}
