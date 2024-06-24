eeccf6cd51d3f33b1600fe3aaab39ea1242071ba
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-24 17:05:11 +0530

    javafx basics

--- Anju/Basics/src/main/java/AreaChart/MainFormController.java
+++ Anju/Basics/src/main/java/AreaChart/MainFormController.java
@@ -1,13 +1,38 @@
 package AreaChart;
 
+import javafx.fxml.FXML;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 
 public class MainFormController {
-    public AreaChart<Number,Number> areaChart;
 
-    NumberAxis xAxis = new NumberAxis(1,7,1);
-    NumberAxis yAxis = new NumberAxis(10,500,20);
+    @FXML
+    private AreaChart<Number, Number> areaChart;
 
-    public void initialize() {}
+    private NumberAxis xAxis;
+    private NumberAxis yAxis;
+
+    public MainFormController() {
+        xAxis = new NumberAxis(1, 7, 1);
+        yAxis = new NumberAxis(10, 500, 20);
+    }
+
+    @FXML
+    public void initialize() {
+        areaChart.setTitle("Student Performance");
+
+        XYChart.Series<Number, Number> javaStudent = new XYChart.Series<>();
+        javaStudent.setName("Java");
+
+        javaStudent.getData().add(new XYChart.Data<>(1, 20));
+        javaStudent.getData().add(new XYChart.Data<>(2, 45));
+        javaStudent.getData().add(new XYChart.Data<>(3, 67));
+        javaStudent.getData().add(new XYChart.Data<>(4, 89));
+        javaStudent.getData().add(new XYChart.Data<>(5, 45));
+        javaStudent.getData().add(new XYChart.Data<>(6, 120));
+        javaStudent.getData().add(new XYChart.Data<>(7, 95));
+
+        areaChart.getData().addAll(javaStudent);
+    }
 }
