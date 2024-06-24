a5974efc2930951fd23397f6613af5c053753d17
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-24 14:53:55 +0530

    javafx basics

--- Anju/Basics/src/main/java/KeyPressing/MainFormController.java
+++ Anju/Basics/src/main/java/KeyPressing/MainFormController.java
@@ -1,4 +1,14 @@
 package KeyPressing;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
+    public TextField txtKeyPress;
+    public Label lblKeyPressAnswer;
+
+    public void keyPressOnAction(ActionEvent actionEvent) {
+
+    }
 }
