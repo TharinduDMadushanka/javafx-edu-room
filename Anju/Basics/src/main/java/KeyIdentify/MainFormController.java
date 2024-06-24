b739ed89bf0bab8dc12b7c0f66e4ce860f99fda6
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-24 15:56:06 +0530

    javafx basics

--- Anju/Basics/src/main/java/KeyIdentify/MainFormController.java
+++ Anju/Basics/src/main/java/KeyIdentify/MainFormController.java
@@ -1,14 +1,22 @@
 package KeyIdentify;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainFormController{
 
+    public TextField txtInput;
+    public TextField txtOutput;
+
     public void initialize() {
-        System.out.println("Hello");
+        //add a listener
+        txtInput.textProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("New Value: " + newValue);
+            System.out.println("Old Value: " + oldValue);
+        });
     }
 
 
