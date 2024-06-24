e1c6bc4bfd6d21d6275b15f9edc3179496542810
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-24 13:43:52 +0530

    javafx basics

--- Anju/Basics/src/main/java/first/AppInitializer.java
+++ Anju/Basics/src/main/java/first/AppInitializer.java
@@ -1,7 +1,15 @@
 package first;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
 
     }
 }
