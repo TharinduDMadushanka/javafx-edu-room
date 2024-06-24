49a332e9393836beaec9eb4f082cca9d1f7f5ce3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-24 16:13:03 +0530

    javafx basics

--- Anju/Basics/src/main/java/ComboBox/MainFormController.java
+++ Anju/Basics/src/main/java/ComboBox/MainFormController.java
@@ -1,16 +1,21 @@
 package ComboBox;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainFormController {
-    public ComboBox<String> cmbLanguages;
+    //public ComboBox<String> cmbLanguages;
+    public ComboBox cmbLanguages;
 
     ArrayList<String> languages = new ArrayList<>(
             Arrays.asList("Java","Python","Ruby","JS")
     );
+
+
 
     public void initialize(){
 
@@ -19,9 +24,13 @@ public class MainFormController {
 //        cmbLanguages.getItems().add("Ruby");
 //        cmbLanguages.getItems().add("JS");
 
-        for(String lang : languages){
-            cmbLanguages.getItems().add(lang);
-        }
+//        for(String lang : languages){
+//            cmbLanguages.getItems().add(lang);
+//        }
+
+        ObservableList<String> items = FXCollections.observableArrayList(languages);
+
+        cmbLanguages.setItems(items);
 
     }
 
