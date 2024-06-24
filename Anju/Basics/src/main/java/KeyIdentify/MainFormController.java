433af99dc63675d829bb0417b8b9741ec3d14237
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-24 15:26:30 +0530

    javafx basics

--- Anju/Basics/src/main/java/KeyIdentify/MainFormController.java
+++ Anju/Basics/src/main/java/KeyIdentify/MainFormController.java
@@ -1,9 +1,29 @@
 package KeyIdentify;
 
-public class MainFormController {
+import javafx.fxml.Initializable;
 
-    MainFormController(){
-        System.out.println("Hello");
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainFormController implements Initializable { //functional interface -- > have only one method
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("hello");
     }
+
+    //Error
+//    MainFormController(){
+//        System.out.println("Hello");
+//    }
+
+    //OK
+//    static {
+//        System.out.println("Hello");
+//    }
+
+    // OK
+//    {
+//        System.out.println("Hello");
+//    }
 
 }
