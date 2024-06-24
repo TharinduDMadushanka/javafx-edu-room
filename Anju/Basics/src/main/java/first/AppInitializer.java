c255605af1a8378df32114f65436940e136c3624
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-24 13:47:50 +0530

    javafx basics

--- Anju/Basics/src/main/java/first/AppInitializer.java
+++ Anju/Basics/src/main/java/first/AppInitializer.java
@@ -3,6 +3,7 @@ package first;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -21,6 +22,8 @@ public class AppInitializer extends Application {
         //load to the RAM
         Parent load = FXMLLoader.load(resource);
 
+        // create a scene
+        Scene scene = new Scene(load);
 
     }
 }
