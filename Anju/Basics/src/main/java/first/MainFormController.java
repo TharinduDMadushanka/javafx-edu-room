4c459d08999d64e54e904185a32c7d812b049532
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-24 13:58:55 +0530

    javafx basics

--- Anju/Basics/src/main/java/first/MainFormController.java
+++ Anju/Basics/src/main/java/first/MainFormController.java
@@ -1,8 +1,14 @@
 package first;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 
 public class MainFormController {
+    public Label lblName;
+
     public void clickMeOnAction(ActionEvent actionEvent) {
+
+
+
     }
 }
