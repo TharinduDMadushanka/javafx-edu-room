13504cb576a2834296601fba1818c9dc80251105
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-24 15:04:27 +0530

    javafx basics

--- Anju/Basics/src/main/java/KeyPressing/MainFormController.java
+++ Anju/Basics/src/main/java/KeyPressing/MainFormController.java
@@ -7,11 +7,17 @@ import javafx.scene.input.KeyEvent;
 public class MainFormController {
     public TextField txtKeyPress;
     public Label lblKeyPressAnswer;
+    public TextField txtKeyRelease;
+    public Label lblKeyReleaseAnswer;
 
     public void keyPressOnAction(KeyEvent actionEvent) {
 
         String value = txtKeyPress.getText(); // get old value
         lblKeyPressAnswer.setText(value);
 
+    }
+
+    public void onKeyRelease(KeyEvent keyEvent) {
+        String value = txtKeyRelease.getText();
     }
 }
