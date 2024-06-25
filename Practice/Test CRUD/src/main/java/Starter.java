e314f58b1510a09727db07d8c2988bdb7aa8bab3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-25 21:59:07 +0530

    test crud operation

---
+++ Practice/Test CRUD/src/main/java/Starter.java
@@ -0,0 +1,7 @@
+import lk.practice.controller.initializer.AppInitializer;
+
+public class Starter{
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
---
+++ Practice/Test CRUD/src/main/java/lk/practice/controller/MainFormController.java
@@ -0,0 +1,4 @@
+package lk.practice.controller;
+
+public class MainFormController{
+}
