41af02701970b9591667019cbb18da6c7d114d6b
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-25 19:00:55 +0530

    javafx splash screen

---
+++ Practice/Styling/src/main/java/Starter.java
@@ -0,0 +1,7 @@
+import lk.practice.controller.AppInitializer;
+
+public class Starter {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
---
+++ Practice/Styling/src/main/java/lk/practice/controller/MainFormController.java
@@ -0,0 +1,9 @@
+package lk.practice.controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFormController {
+    public AnchorPane rootNode;
+    public Button btnSave;
+}
