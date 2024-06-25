c3e98ba965696c54b314b1cfd3e6c67fab427f34
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-25 09:54:06 +0530

    javafx splash screen

---
+++ Anju/Basics/src/main/java/Style/MainFormController.java
@@ -0,0 +1,2 @@
+package Style;public class MainFormController {
+}
---
+++ Practice/Splash Screen/src/main/java/lk/practice/controller/SplashScreenController.java
@@ -0,0 +1,10 @@
+package lk.practice.controller;
+
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+
+public class SplashScreenController {
+    public Rectangle recMain;
+    public Rectangle recSub;
+    public Label lblProgress;
+}
