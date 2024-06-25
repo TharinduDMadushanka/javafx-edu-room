09bb6d9f6e2ed2ceb7d44e34736ab61ed22d37eb
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-25 08:25:23 +0530

    javafx basics

--- Anju/Basics/src/main/java/NavigationType03/DashboardController.java
+++ Anju/Basics/src/main/java/NavigationType03/DashboardController.java
@@ -9,13 +9,24 @@ import java.io.IOException;
 public class DashboardController {
     public AnchorPane loadContex;
 
+    public void initialize() throws IOException {
+        setUI("/NavigationType03/WindowA");
+    }
+
     public void windowAOnAction(ActionEvent actionEvent) throws IOException {
-        loadContex.getChildren().clear();
-        loadContex.getChildren().add(FXMLLoader.load(getClass().getResource("/NavigationType03/WindowA.fxml")));
+//        loadContex.getChildren().clear();
+//        loadContex.getChildren().add(FXMLLoader.load(getClass().getResource("/NavigationType03/WindowA.fxml")));
+        setUI("/NavigationType03/WindowA");
     }
 
     public void windowBOnAction(ActionEvent actionEvent) throws IOException {
+//        loadContex.getChildren().clear();
+//        loadContex.getChildren().add(FXMLLoader.load(getClass().getResource("/NavigationType03/WindowB.fxml")));
+        setUI("/NavigationType03/WindowB");
+    }
+
+    private void setUI(String location) throws IOException {
         loadContex.getChildren().clear();
-        loadContex.getChildren().add(FXMLLoader.load(getClass().getResource("/NavigationType03/WindowB.fxml")));
+        loadContex.getChildren().add(FXMLLoader.load(getClass().getResource(location+".fxml")));
     }
 }
