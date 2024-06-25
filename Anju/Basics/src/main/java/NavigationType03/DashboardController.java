402c859ec9ef93263dd844d34d6dc2b7a5ed3ec4
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-25 08:19:51 +0530

    javafx basics

--- Anju/Basics/src/main/java/NavigationType03/DashboardController.java
+++ Anju/Basics/src/main/java/NavigationType03/DashboardController.java
@@ -1,14 +1,19 @@
 package NavigationType03;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class DashboardController {
     public AnchorPane loadContex;
 
-    public void windowAOnAction(ActionEvent actionEvent) {
+    public void windowAOnAction(ActionEvent actionEvent) throws IOException {
+        loadContex.getChildren().add(FXMLLoader.load(getClass().getResource("/NavigationType03/WindowA.fxml")));
     }
 
-    public void windowBOnAction(ActionEvent actionEvent) {
+    public void windowBOnAction(ActionEvent actionEvent) throws IOException {
+        loadContex.getChildren().add(FXMLLoader.load(getClass().getResource("/NavigationType03/WindowB.fxml")));
     }
 }
