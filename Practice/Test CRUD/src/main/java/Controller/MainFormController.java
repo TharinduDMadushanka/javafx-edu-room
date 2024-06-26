f636bc6f952379f13848a1bce37aee88c07ba204
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-26 08:56:26 +0530

    test crud operation

--- Practice/Test CRUD/src/main/java/Controller/MainFormController.java
+++ Practice/Test CRUD/src/main/java/Controller/MainFormController.java
@@ -1,9 +1,14 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+
 public class MainFormController{
+
+    private StudentController studentController;
     public TextField txtName;
     public TextField txtGrade;
     public TextField txtAge;
@@ -11,4 +16,30 @@ public class MainFormController{
     public Button btnUpdate;
     public Button btnDelete;
     public Button btnClear;
+
+    public MainFormController() throws Exception {
+        studentController=new StudentController();
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+
+        try{
+
+        }
+
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnClearOnAction(ActionEvent actionEvent) {
+        txtName.clear();
+        txtGrade.clear();
+        txtAge.clear();
+    }
 }
