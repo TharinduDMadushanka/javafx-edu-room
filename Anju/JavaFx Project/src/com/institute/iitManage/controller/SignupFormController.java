ebe5c4d8a4607bb933e3e91ea8e155deae2759cf
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-30 04:04:39 +0530

    create signup form ui

---
+++ Anju/JavaFx Project/src/com/institute/iitManage/controller/SignupFormController.java
@@ -0,0 +1,19 @@
+package com.institute.iitManage.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SignupFormController {
+    public AnchorPane context;
+    public TextField txtFirstName;
+    public TextField txtEmail;
+    public TextField txtLastName;
+    public TextField txtPassword;
+
+    public void signupOnAction(ActionEvent actionEvent) {
+    }
+
+    public void alreadyHaveAccountOnAction(ActionEvent actionEvent) {
+    }
+}
