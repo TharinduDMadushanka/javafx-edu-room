87168becd90395907fac3f4ed7c5fbb30a5e3681
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-30 04:12:35 +0530

    Navigated to LoginForm when click on already have an account

--- Anju/JavaFx Project/src/com/institute/iitManage/controller/SignupFormController.java
+++ Anju/JavaFx Project/src/com/institute/iitManage/controller/SignupFormController.java
@@ -1,8 +1,13 @@
 package com.institute.iitManage.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SignupFormController {
     public AnchorPane context;
@@ -14,6 +19,15 @@ public class SignupFormController {
     public void signupOnAction(ActionEvent actionEvent) {
     }
 
-    public void alreadyHaveAccountOnAction(ActionEvent actionEvent) {
+    public void alreadyHaveAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("LoginForm");
+    }
+
+
+    private void setUI(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
     }
 }
