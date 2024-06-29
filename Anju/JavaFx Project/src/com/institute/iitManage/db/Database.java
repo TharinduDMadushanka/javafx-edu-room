c28adee215ec1e4bef9ede01e951b2cb2164a68f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-30 04:59:13 +0530

    add default user via static block in database class

--- Anju/JavaFx Project/src/com/institute/iitManage/db/Database.java
+++ Anju/JavaFx Project/src/com/institute/iitManage/db/Database.java
@@ -7,4 +7,13 @@ import java.util.ArrayList;
 public class Database {
 
     public static ArrayList<User> userTable = new ArrayList();
+
+    static {
+        userTable.add(new User(
+                "Amal",
+                "Perera",
+                "amal@gmail.com",
+                "1234"
+        ));
+    }
 }
