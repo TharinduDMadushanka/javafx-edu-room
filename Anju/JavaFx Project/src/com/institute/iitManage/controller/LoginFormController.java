0186b8d0c7b3a51f61c090dbee1b3ed270dfec68
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-30 03:50:28 +0530

    create logingform UI and genarate components ids in controller class

--- Anju/JavaFx Project/src/com/institute/iitManage/controller/LoginFormController.java
+++ Anju/JavaFx Project/src/com/institute/iitManage/controller/LoginFormController.java
@@ -1,4 +1,19 @@
 package com.institute.iitManage.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane context;
+    public TextField txtEmail;
+    public TextField txtPassword;
+    public Hyperlink txtForgotPw;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void createAccountOnAction(ActionEvent actionEvent) {
+    }
 }
