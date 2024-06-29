635bae44f61b4065d6b9905a41f41e8fa5e26fdf
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-30 04:10:12 +0530

    created setUI method and navigated to SignupForm

--- Anju/JavaFx Project/src/com/institute/iitManage/controller/LoginFormController.java
+++ Anju/JavaFx Project/src/com/institute/iitManage/controller/LoginFormController.java
@@ -1,9 +1,14 @@
 package com.institute.iitManage.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public AnchorPane context;
@@ -14,6 +19,14 @@ public class LoginFormController {
     public void loginOnAction(ActionEvent actionEvent) {
     }
 
-    public void createAccountOnAction(ActionEvent actionEvent) {
+    public void createAccountOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("SignupForm");
+    }
+
+    private void setUI(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.show();
+        stage.centerOnScreen();
     }
 }
