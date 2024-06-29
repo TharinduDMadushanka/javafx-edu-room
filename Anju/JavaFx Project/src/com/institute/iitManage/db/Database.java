f5f477573d0cea028c1d5f142f851dccdce171a7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-30 04:20:44 +0530

    created db package and Database class

--- Anju/JavaFx Project/src/com/institute/iitManage/db/Database.java
+++ Anju/JavaFx Project/src/com/institute/iitManage/db/Database.java
@@ -1,4 +1,10 @@
 package com.institute.iitManage.db;
 
+import com.institute.iitManage.model.User;
+
+import java.util.ArrayList;
+
 public class Database {
+
+    public static ArrayList<User> userTable = new ArrayList();
 }
