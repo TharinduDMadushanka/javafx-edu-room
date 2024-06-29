9d7a7c19e1796a0207f3ea7c421eb8f6c5177a8f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-30 05:25:58 +0530

    creat dashboard UI and genarate components in dashboard controller class

---
+++ Anju/JavaFx Project/src/com/institute/iitManage/controller/DashBoardController.java
@@ -0,0 +1,30 @@
+package com.institute.iitManage.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class DashBoardController {
+    public AnchorPane context;
+    public Label lblDate;
+    public Label lblTime;
+
+    public void studentsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void teacherOnAction(ActionEvent actionEvent) {
+    }
+
+    public void intakeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void courseOnAction(ActionEvent actionEvent) {
+    }
+
+    public void registrationOnAction(ActionEvent actionEvent) {
+    }
+
+
+    public void logoutOnAction(ActionEvent actionEvent) {
+    }
+}
