31d86d1592b663d4afa84fc823c30423cdd6a7dd
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-01 14:37:24 +0530

    create student table and student mode

--- Anju/JavaFx Project/src/com/institute/iitManage/db/Database.java
+++ Anju/JavaFx Project/src/com/institute/iitManage/db/Database.java
@@ -1,5 +1,6 @@
 package com.institute.iitManage.db;
 
+import com.institute.iitManage.model.Student;
 import com.institute.iitManage.model.User;
 import com.institute.iitManage.util.security.PasswordManager;
 
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 public class Database {
 
     public static ArrayList<User> userTable = new ArrayList();
+    public static ArrayList<Student> studentTable = new ArrayList();
 
     static {
         userTable.add(new User(
