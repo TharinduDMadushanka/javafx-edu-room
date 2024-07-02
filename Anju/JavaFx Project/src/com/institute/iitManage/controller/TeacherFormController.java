f032be55d827fff712bb3e89524ae421d363c3ba
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-02 09:12:25 +0530

    complete TeacherUI and navigate all componenet in teacher controllere class

--- Anju/JavaFx Project/src/com/institute/iitManage/controller/TeacherFormController.java
+++ Anju/JavaFx Project/src/com/institute/iitManage/controller/TeacherFormController.java
@@ -1,4 +1,33 @@
 package com.institute.iitManage.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class TeacherFormController {
+    public AnchorPane context;
+    public TextField txtTeacherID;
+    public TextField txtFullName;
+    public TextField txtAddress;
+    public Button btnSaveTeacher;
+    public TextField txtSearch;
+    public TableView tblTeacher;
+    public TableColumn colTeacherID;
+    public TableColumn colFullName;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colOption;
+    public TextField txtContact;
+
+    public void newTeacherOnAction(ActionEvent actionEvent) {
+    }
+
+    public void backToHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void saveTeacherOnAction(ActionEvent actionEvent) {
+    }
 }
