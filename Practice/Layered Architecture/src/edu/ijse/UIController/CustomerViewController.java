e2d56952fc97089a61e6a85b92980e5ef7013a71
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-06 21:15:10 +0530

    complete CustomerViewController class

--- Practice/Layered Architecture/src/edu/ijse/UIController/CustomerViewController.java
+++ Practice/Layered Architecture/src/edu/ijse/UIController/CustomerViewController.java
@@ -10,6 +10,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 
 import java.util.ArrayList;
 
@@ -26,17 +27,17 @@ public class CustomerViewController {
 
     public TableView<CustomerDto> customerTable;
     public TableColumn<CustomerDto,String> colId;
-    public TableColumn<CustomerDto,String>  colTitle;
-    public TableColumn<CustomerDto,String>  colName;
-    public TableColumn<CustomerDto,String>  colDoB;
-    public TableColumn<CustomerDto,String>  colSalary;
-    public TableColumn<CustomerDto,String>  colAddress;
-    public TableColumn<CustomerDto,String>  colCity;
-    public TableColumn<CustomerDto,String>  colProvince;
-    public TableColumn<CustomerDto,String>  colPostal;
+    public TableColumn<CustomerDto,String> colTitle;
+    public TableColumn<CustomerDto,String> colName;
+    public TableColumn<CustomerDto,String> colDoB;
+    public TableColumn<CustomerDto,String> colSalary;
+    public TableColumn<CustomerDto,String> colAddress;
+    public TableColumn<CustomerDto,String> colCity;
+    public TableColumn<CustomerDto,String> colProvince;
+    public TableColumn<CustomerDto,String> colPostal;
 
-    private CustomerServiceImpl customerService=new CustomerServiceImpl();
-    private ObservableList<CustomerDto> customerList = FXCollections.observableArrayList();
+    private final CustomerServiceImpl customerService = new CustomerServiceImpl();
+    private final ObservableList<CustomerDto> customerList = FXCollections.observableArrayList();
 
     public void initialize() {
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
@@ -49,8 +50,10 @@ public class CustomerViewController {
         colProvince.setCellValueFactory(new PropertyValueFactory<>("province"));
         colPostal.setCellValueFactory(new PropertyValueFactory<>("postal"));
 
-        loadTable();
+        loadCustomer();
 
+        // Add listener to the table selection
+        customerTable.setOnMouseClicked(this::selectValue);
     }
 
     public void saveOnAction(ActionEvent actionEvent) {
@@ -68,25 +71,77 @@ public class CustomerViewController {
 
         try {
             String result = customerService.save(customer);
-            if (result.equals("Success")) {
+            if ("Success".equals(result)) {
                 customerList.add(customer);
                 customerTable.refresh();
+                clearFields();
                 new Alert(Alert.AlertType.INFORMATION, "Customer Saved...!").showAndWait();
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            new Alert(Alert.AlertType.ERROR, "Customer Save Failed...").showAndWait();
+            new Alert(Alert.AlertType.ERROR, "Customer Save Failed...").show();
         }
-
     }
 
     public void updateOnAction(ActionEvent actionEvent) {
+        CustomerDto customer = new CustomerDto(
+                txtId.getText(),
+                txtTitle.getText(),
+                txtName.getText(),
+                txtDob.getText(),
+                Double.parseDouble(txtSalary.getText()),
+                txtAddress.getText(),
+                txtCity.getText(),
+                txtProvince.getText(),
+                txtPostal.getText()
+        );
+
+        try {
+            String result = customerService.update(customer);
+            if ("Success".equals(result)) {
+                loadCustomer();
+                clearFields();
+                new Alert(Alert.AlertType.INFORMATION, "Customer Updated...!").showAndWait();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Customer Update Failed...").show();
+        }
     }
 
     public void deleteOnAction(ActionEvent actionEvent) {
+        String customerId = txtId.getText();
+
+        try {
+            String result = customerService.delete(customerId);
+            if ("Success".equals(result)) {
+                loadCustomer();
+                clearFields();
+                new Alert(Alert.AlertType.INFORMATION, "Customer Deleted...!").showAndWait();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Customer Delete Failed...").show();
+        }
     }
 
     public void clearOnAction(ActionEvent actionEvent) {
+        clearFields();
+    }
+
+    private void loadCustomer() {
+        try {
+            ArrayList<CustomerDto> customers = customerService.getAll();
+            if (customers != null) {
+                customerList.setAll(customers);
+                customerTable.setItems(customerList);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clearFields() {
         txtId.clear();
         txtTitle.clear();
         txtName.clear();
@@ -98,15 +153,18 @@ public class CustomerViewController {
         txtPostal.clear();
     }
 
-    private void loadTable(){
-        try {
-            ArrayList<CustomerDto> customers = customerService.getAll();
-            if(customers != null){
-                customerList.addAll(customers);
-                customerTable.setItems(customerList);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
+    private void selectValue(MouseEvent mouseEvent) {
+        CustomerDto selectedCustomer = customerTable.getSelectionModel().getSelectedItem();
+        if (selectedCustomer != null) {
+            txtId.setText(selectedCustomer.getId());
+            txtTitle.setText(selectedCustomer.getTitle());
+            txtName.setText(selectedCustomer.getName());
+            txtDob.setText(selectedCustomer.getDob());
+            txtSalary.setText(String.valueOf(selectedCustomer.getSalary()));
+            txtAddress.setText(selectedCustomer.getAddress());
+            txtCity.setText(selectedCustomer.getCity());
+            txtProvince.setText(selectedCustomer.getProvince());
+            txtPostal.setText(selectedCustomer.getPostal());
         }
     }
 }
