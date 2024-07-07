222f79d3adc692e1027c6f9bc93821f6fc8314b3
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-07 19:14:13 +0530

    complete OrderDetailEntity class

--- Practice/Layered Architecture/src/edu/ijse/entity/OrderDetailEntity.java
+++ Practice/Layered Architecture/src/edu/ijse/entity/OrderDetailEntity.java
@@ -2,5 +2,60 @@ package edu.ijse.entity;
 
 public class OrderDetailEntity {
 
+    private String orderId;
+    private String itemCode;
+    private int qty;
+    private int discount;
 
+    public OrderDetailEntity() {
+    }
+
+    public OrderDetailEntity(String orderId, String itemCode, int qty, int discount) {
+        this.orderId = orderId;
+        this.itemCode = itemCode;
+        this.qty = qty;
+        this.discount = discount;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetailEntity{" +
+                "orderId='" + orderId + '\'' +
+                ", itemCode='" + itemCode + '\'' +
+                ", qty=" + qty +
+                ", discount=" + discount +
+                '}';
+    }
 }
