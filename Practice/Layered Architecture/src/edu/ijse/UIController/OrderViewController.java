1cc7ee8adf8072104cc7d5baac11da5f56c4fa0b
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-07 21:13:22 +0530

    complete customerSearchOnAction

--- Practice/Layered Architecture/src/edu/ijse/UIController/OrderViewController.java
+++ Practice/Layered Architecture/src/edu/ijse/UIController/OrderViewController.java
@@ -1,25 +1,59 @@
 package edu.ijse.UIController;
 
+import edu.ijse.dto.CustomerDto;
+import edu.ijse.dto.OrderDetailDto;
+import edu.ijse.dto.OrderDto;
+import edu.ijse.service.custom.impl.CustomerServiceImpl;
+import edu.ijse.service.custom.impl.ItemServiceImpl;
+import edu.ijse.service.custom.impl.OrderServiceImpl;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 public class OrderViewController {
-    public AnchorPane txtCustomerId;
+    public AnchorPane rootPane;
     public TextField txtOrderId;
+    public TextField txtCustomerIdInput;
     public TextField txtCustomerShow;
     public TextField txtItemId;
     public TextField txtDiscount;
     public TextField txtItemShow;
     public TextField txtQty;
-    public TableView tblOrder;
-    public TableColumn colItemCode;
-    public TableColumn colQty;
-    public TableColumn colDiscount;
+    public TableView<OrderDetailDto> tblOrder;
+    public TableColumn<OrderDetailDto, String> colItemCode;
+    public TableColumn<OrderDetailDto, Integer> colQty;
+    public TableColumn<OrderDetailDto, Integer> colDiscount;
+
+    private CustomerServiceImpl customerService = new CustomerServiceImpl();
+    private ItemServiceImpl itemService = new ItemServiceImpl();
+    private OrderServiceImpl orderService = new OrderServiceImpl();
+    private ObservableList<OrderDetailDto> orderDetails = FXCollections.observableArrayList();
+
+    public void initialize() {
+        colItemCode.setCellValueFactory(new PropertyValueFactory<>("itemCode"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colDiscount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+    }
 
     public void customerSearchOnAction(ActionEvent actionEvent) {
+        try{
+            String customerId = txtCustomerIdInput.getText();
+            CustomerDto customer = customerService.getCustomer(customerId);
+
+            if(customer != null){
+                txtCustomerShow.setText(customer.getTitle()+". "+customer.getName()+" | "+customer.getCity());
+            }else {
+                txtCustomerShow.setText("Customer not fount!");
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void itemSearchOnAction(ActionEvent actionEvent) {
