bb41263f3edb86109707ac35fb7e5d1e30aa7674
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-07 21:18:49 +0530

    complete itemAddOnAction

--- Practice/Layered Architecture/src/edu/ijse/UIController/OrderViewController.java
+++ Practice/Layered Architecture/src/edu/ijse/UIController/OrderViewController.java
@@ -10,6 +10,7 @@ import edu.ijse.service.custom.impl.OrderServiceImpl;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -75,6 +76,26 @@ public class OrderViewController {
     }
 
     public void itemAddOnAction(ActionEvent actionEvent) {
+        try {
+
+            String itemId = txtItemId.getText();
+            int qty = Integer.parseInt(txtQty.getText());
+            int discount = Integer.parseInt(txtDiscount.getText());
+
+            OrderDetailDto orderDetail = new OrderDetailDto("",itemId,qty,discount);
+            orderDetails.add(orderDetail);
+
+            new Alert(Alert.AlertType.INFORMATION,"Successfully added item!").show();
+
+            txtItemId.clear();
+            txtQty.clear();
+            txtDiscount.clear();
+            txtItemShow.clear();
+
+        }catch (Exception e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.INFORMATION,"Failed to add item!").show();
+        }
     }
 
     public void placeOrderOnAction(ActionEvent actionEvent) {
