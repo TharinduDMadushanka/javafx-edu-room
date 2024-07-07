2a83abf3d69c2c0863ffe31aa4b1368e13e7ec48
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-07 19:13:29 +0530

    complete OrderEntity class

--- Practice/Layered Architecture/src/edu/ijse/entity/OrderEntity.java
+++ Practice/Layered Architecture/src/edu/ijse/entity/OrderEntity.java
@@ -1,4 +1,49 @@
 package edu.ijse.entity;
 
 public class OrderEntity {
+    private String orderId;
+    private String custId;
+    private String date;
+
+    public OrderEntity() {
+    }
+
+    public OrderEntity(String orderId, String custId, String date) {
+        this.orderId = orderId;
+        this.custId = custId;
+        this.date = date;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetailEntity{" +
+                "orderId='" + orderId + '\'' +
+                ", custId='" + custId + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
 }
