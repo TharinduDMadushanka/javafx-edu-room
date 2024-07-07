a3c0b8abcb9a43cef14d22f398f3484bd1a9852b
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-07 19:26:39 +0530

    add quary

--- Practice/Layered Architecture/src/edu/ijse/dao/custom/impl/OrderDetailDaoImpl.java
+++ Practice/Layered Architecture/src/edu/ijse/dao/custom/impl/OrderDetailDaoImpl.java
@@ -1,5 +1,6 @@
 package edu.ijse.dao.custom.impl;
 
+import edu.ijse.dao.CrudUtil;
 import edu.ijse.dao.custom.OrderDetailDao;
 import edu.ijse.entity.OrderDetailEntity;
 
@@ -7,8 +8,8 @@ import java.util.ArrayList;
 
 public class OrderDetailDaoImpl implements OrderDetailDao {
     @Override
-    public boolean create(OrderDetailEntity orderDetailEntity) throws Exception {
-        return false;
+    public boolean create(OrderDetailEntity t) throws Exception {
+        return CrudUtil.executeUpdate("INSERT INTO OrderDetail VALUES(?,?,?,?)",t.getOrderId(),t.getItemCode(),t.getQty(),t.getDiscount());
     }
 
     @Override
